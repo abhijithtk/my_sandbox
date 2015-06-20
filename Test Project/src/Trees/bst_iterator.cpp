a8d4f7fd117299e0d178f76779292392c3aa18bf
abhijithtk <abhi.tk@gmail.com>
2015-06-20 12:51:28 -0400

    Create bst_iterator.cpp

---
+++ Test Project/src/Trees/bst_iterator.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+public class BSTIterator {
+	Stack<TreeNode> stack;
+ 
+	public BSTIterator(TreeNode root) {
+		stack = new Stack<TreeNode>();
+		while (root != null) {
+			stack.push(root);
+			root = root.left;
+		}
+	}
+ 
+	public boolean hasNext() {
+		return !stack.isEmpty();
+	}
+ 
+	public int next() {
+		TreeNode node = stack.pop();
+		int result = node.val;
+		if (node.right != null) {
+			node = node.right;
+			while (node != null) {
+				stack.push(node);
+				node = node.left;
+			}
+		}
+		return result;
+	}
+}
