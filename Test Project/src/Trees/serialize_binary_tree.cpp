7375da2eba5c9240d9cc9668aaccb01f08348ea7
abhijithtk <abhi.tk@gmail.com>
2015-06-26 18:24:37 -0400

    Create serialize_binary_tree.cpp

---
+++ Test Project/src/Trees/serialize_binary_tree.cpp
@@ -0,0 +1,22 @@
+
+void writeBinaryTree(BinaryTree *p, ostream &out) {
+  if (!p) {
+    out << "# ";
+  } else {
+    out << p->data << " ";
+    writeBinaryTree(p->left, out);
+    writeBinaryTree(p->right, out);
+  }
+}
+
+void readBinaryTree(BinaryTree *&p, ifstream &fin) {
+  int token;
+  bool isNumber;
+  if (!readNextToken(token, fin, isNumber)) 
+    return;
+  if (isNumber) {
+    p = new BinaryTree(token);
+    readBinaryTree(p->left, fin);
+    readBinaryTree(p->right, fin);
+  }
+}
