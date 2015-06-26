1d0b4ea5e403a83a8d5785eb692c15d414f089e0
abhijithtk <abhi.tk@gmail.com>
2015-06-26 18:42:41 -0400

    Update serialize_binary_tree.cpp

--- Test Project/src/Trees/serialize_binary_tree.cpp
+++ Test Project/src/Trees/serialize_binary_tree.cpp
@@ -20,3 +20,22 @@ void readBinaryTree(BinaryTree *&p, ifstream &fin) {
     readBinaryTree(p->right, fin);
   }
 }
+
+// Below is for BST only. writing to file is a simple pre order write
+void readBSTHelper(int min, int max, int &insertVal,
+                   BinaryTree *&p, ifstream &fin) {
+  if (insertVal > min && insertVal < max) {
+    int val = insertVal;
+    p = new BinaryTree(val);
+    if (fin >> insertVal) {
+      readBSTHelper(min, val, insertVal, p->left, fin);
+      readBSTHelper(val, max, insertVal, p->right, fin);
+    }
+  }
+}
+ 
+void readBST(BinaryTree *&root, ifstream &fin) {
+  int val;
+  fin >> val;
+  readBSTHelper(INT_MIN, INT_MAX, val, root, fin);
+}
