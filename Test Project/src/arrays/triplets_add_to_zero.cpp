a1353c594beb6e27ee6d0f2491636740dfdb640b
Abhijith Tirumala Kumara <abhi.tk@gmail.com>
2016-05-17 14:53:28 -0700

    Create triplets_add_to_zero.cpp

---
+++ Test Project/src/arrays/triplets_add_to_zero.cpp
@@ -0,0 +1,52 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+set<vector<int> > find_triplets(vector<int> arr) {
+  sort(arr.begin(), arr.end());
+  set<vector<int> > triplets;
+  vector<int> triplet(3);
+  int n = arr.size();
+  for (int i = 0;i < n; i++) {
+    int j = i + 1;
+    int k = n - 1;
+    while (j < k) {
+      int sum_two = arr[i] + arr[j];
+      if (sum_two + arr[k] < 0) {
+        j++;
+      } else if (sum_two + arr[k] > 0) {
+        k--;
+      } else {
+        triplet[0] = arr[i];
+        triplet[1] = arr[j];
+        triplet[2] = arr[k];
+        triplets.insert(triplet);
+        j++;
+        k--;
+      }
+    }
+  }
+  return triplets;
+}
