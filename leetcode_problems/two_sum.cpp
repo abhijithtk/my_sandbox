d7caad8807deb11a7372f6cd0a3da16805fed2c9
abhijith <abh@amazon.com>
2017-10-18 12:16:06 -0700

    fixing two sum

--- leetcode_problems/two_sum.cpp
+++ leetcode_problems/two_sum.cpp
@@ -8,27 +8,32 @@ using namespace std;
 
 class Solution {
 public:
-    int solve(vector<int>& board) {
-    	int target  = 18;
+    vector<int> solve(vector<int>& board) {
+    	int target  = 9;
+    	vector<int> v;
     	map<int, int> m;
     	for (int i = 0; i < board.size(); i++) {
     		int diff = target - board[i];
     		if(m.find(diff) != m.end()) {
-    			cout << "Found" << diff << " at " << m[diff] << " and found " << board[i] << "at" << i << endl;
+    			v.push_back(m[diff]);
+    			v.push_back(i);
+    			//cout << "Found" << diff << " at " << m[diff] << " and found " << board[i] << "at" << i << endl;
     			break;
     		}
-    		cout << i << endl;
+    		//cout << i << endl;
     		m.insert(make_pair(board[i],i));
 
     	}
-    	return 1;
+    	return v;
     }
 };
 
 int main()
 {
-    vector<int> v{18, 7, 11, 4, 16, 22, 15};
+    vector<int> v{2, 7, 11, 15};
     Solution obj;
     //obj.solve(v);
-    cout << obj.solve(v) << endl;
+    //cout << obj.solve(v) << endl;
+    for(auto &i : obj.solve(v))
+    	cout << i << endl;
 }
