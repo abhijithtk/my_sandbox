727dd1a9336a7f0dc7889c9d02e252ac2dba3295
abhijith <abh@amazon.com>
2017-10-19 00:13:37 -0700

    Add two lists. slightly messy, but works

---
+++ leetcode_problems/add_two_lists.cpp
@@ -0,0 +1,80 @@
+#include <vector>
+#include <queue>
+#include <map>
+#include <iostream>
+#include <stack>
+#include <list>
+
+using namespace std;
+
+
+
+struct ListNode {
+	int val;
+ 	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    	ListNode *head = NULL;
+    	ListNode *curr = NULL;
+    	ListNode *prev = NULL;
+    	ListNode *pprev = NULL;
+    	if(l1 == NULL)
+    		return l2;
+    	if(l2 == NULL)
+    		return l1;
+    	int carry = 0;
+    	while(l1 != NULL || l2 != NULL) {
+			int tmp  = carry + ((l1) ? l1->val : 0) + ((l2) ? l2->val : 0);
+    		if (tmp >= 10){
+    			carry = 1;
+    			tmp = tmp % 10;
+    		}
+    		else{
+    			carry = 0;
+    		}
+    		if (head == NULL)
+    		{
+    			head = new ListNode(tmp);
+    			curr = head;
+    		}
+    		else {
+    			curr = new ListNode(tmp);
+    			prev->next = curr;
+    		}
+    		prev = curr;
+    		pprev = prev;
+    		if(l1)
+    			l1 = l1->next;
+    		if(l2)
+    			l2 = l2->next;
+    	}
+		if(carry){
+			pprev->next = new ListNode(1);
+		}
+    	return head;
+    }
+};
+
+int main()
+{
+	// ListNode *l1 = new ListNode(2);
+	// l1->next = new ListNode(4);
+	// l1->next->next = new ListNode(3);
+	// ListNode *l2 = new ListNode(5);
+	// l1->next = new ListNode(6);
+	// l1->next->next = new ListNode(4);
+	ListNode *l1 = new ListNode(1);
+	l1->next = new ListNode(8);
+	ListNode *l2 = new ListNode(0);
+    Solution obj;
+    ListNode *l3 = obj.addTwoNumbers(l1, l2);
+    while(l3) {
+    	cout << l3->val << endl;
+    	l3 = l3->next;
+    }
+
+}
