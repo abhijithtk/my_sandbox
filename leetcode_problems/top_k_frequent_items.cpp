a66a269d2cce6d9bc7c759f96e65de4f67b79c90
abhijith <abh@amazon.com>
2017-10-29 14:00:23 -0700

    Improved run time by limiting heap size to size k

--- leetcode_problems/top_k_frequent_items.cpp
+++ leetcode_problems/top_k_frequent_items.cpp
@@ -4,12 +4,13 @@
 #include <iostream>
 #include <stack>
 #include <functional>
+#include <algorithm>
 
 using namespace std;
 class Functor {
 public:
 	bool operator() (const pair<int, int> &p1, const pair<int, int> &p2){
-		return p1.second < p2.second;
+		return p1.second > p2.second;
 	}
 };
 
@@ -20,25 +21,34 @@ public:
     	for(auto &i : nums){
     		m[i]++;
     	}
+
     	priority_queue<pair<int, int>, std::vector<pair<int, int>>, Functor> pq;
     	map<int, int>::const_iterator mit;
     	for(mit = m.begin(); mit != m.end(); mit++){
-    		pq.push(make_pair(mit->first, mit->second));
+    		if(pq.size() < k){
+	    		pq.push(make_pair(mit->first, mit->second));
+    		}
+    		else {
+	    		if((pq.size() >= k) && (pq.top().second < mit->second)) {
+    				pq.pop();
+    				pq.push(make_pair(mit->first, mit->second));
+				}
+    		}
     	}
 
     	std::vector<int> res;
-    	while(!pq.empty() && k>0){
+    	while(!pq.empty()){
     		res.push_back(pq.top().first);
     		pq.pop();
-    		k--;
     	}
+    	reverse(res.begin(), res.end());
     	return res;
    }
 };
 int main()
 {
 	int k =2;
-    vector<int> v{1,1,1,2,2,3};
+    vector<int> v{4,1,-1,2,-1,2,3};
     Solution obj;
     for(auto &i : obj.topKFrequent(v, k)) {
     	cout << i << endl;
