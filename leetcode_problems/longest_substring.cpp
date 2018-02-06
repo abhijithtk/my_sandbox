a16e81b35d5d8276a783e1555d81f2b3294ebcad
abhijith <abh@amazon.com>
2018-02-05 23:25:13 -0800

    Added new solution

--- leetcode_problems/longest_substring.cpp
+++ leetcode_problems/longest_substring.cpp
@@ -13,6 +13,26 @@ using namespace std;
 
 class Solution {
 public:
+    // final solution. keep moving j until you find a duplicate. 
+    // delete all elements until you reach that duplicate element. 
+    // difference between j and i is the longest substring
+     int lengthOfLongestSubstring1(string s) {
+        set<char> ss;
+        int ans = 0, i = 0, j = 0;
+        while (i < s.size() && j < s.size()) {
+            cout << s[j] << endl;
+            // try to extend the range [i, j]
+            if (ss.find(s[j]) == ss.end()){
+                ss.insert(s[j++]);
+                ans = max(ans, j - i);
+            }
+            else {
+                ss.erase(s[i++]);
+            }
+        }
+        return ans;
+    }
+
 	int lengthOfLongestSubstring1(string s) {
 		map<char, int> m;
 		int ans = 0;
